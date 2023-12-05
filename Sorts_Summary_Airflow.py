63ee981ccc08f92b028b76d9cf04452d3c7aeb83
Adun Ajiye <adunajiye@gmail.com>
2023-12-05 18:54:00 +0100

    Aiflow_sorts

--- Sorts_Summary_Airflow.py
+++ Sorts_Summary_Airflow.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from airflow.models import Variable
 from Save_Db_Function.Get_Sorting import save_sorts_summary
 
-with DAG("Getsorts_Data", start_date=datetime(2023, 11, 30),
+with DAG("Getsort_summary_Data", start_date=datetime(2023, 11, 30),
 schedule_interval='@daily', max_active_runs=1, catchup=False) as dag:
 
     Tstc_script_A = PythonOperator(
